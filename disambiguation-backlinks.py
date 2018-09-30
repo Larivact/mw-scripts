499d25fa4515c3852f9088ef14ea505056117d39
Larivact <larivact@users.noreply.github.com>
2018-09-30 09:11:43 +0200

    disambiguation-backlinks.py: follow redirects

--- disambiguation-backlinks.py
+++ disambiguation-backlinks.py
@@ -6,6 +6,14 @@ import mw
 
 aw = mw.Session('https://wiki.archlinux.org/api.php', 'disambiguation-backlinks.py by User:Larivact')
 
+NAMESPACE='0|4|8|10|12|14'
+
 for member in aw.query('categorymembers', list='categorymembers', cmtitle="Category:Disambiguation pages", cmlimit="max"):
-	for backlink in aw.query('backlinks', list='backlinks', bltitle=member['title'], bllimit='max', blnamespace='0|4|8|10|12|14', blfilterredir='nonredirects'):
-		print(backlink['title'])
+	backlinks = []
+	for backlink in aw.query('backlinks', list='backlinks', bltitle=member['title'], bllimit='max', blnamespace=NAMESPACE, blfilterredir='nonredirects', blredirect=''):
+		backlinks.append(backlink['title'])
+	# second query needed because blfilterredir only applies to the second level when blredirect (follow redirects) is used.
+	for backlink in aw.query('backlinks', list='backlinks', bltitle=member['title'], bllimit='max', blnamespace=NAMESPACE, blfilterredir='redirects'):
+		backlinks.remove(backlink['title'])
+	for backlink in backlinks:
+		print(backlink)
